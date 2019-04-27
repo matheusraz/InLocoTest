e50b8fdfde515cd8c4eba6fcd7373250d00fd872
Matheus Raz <mrol@cin.ufpe.br>
2019-04-27 18:00:55 -0300

    mudança para iterativo

--- exec.py
+++ exec.py
@@ -60,9 +60,39 @@ allGames = []
 oneGameCurrently = False
 data = newData(1)
 
+for line in range(len(arqLines)):
+
+    if('InitGame' in arqLines[line] and not oneGameCurrently):
+        print('COMECEI UM JOGO')
+        oneGameCurrently = True
+    
+    elif('InitGame' in arqLines[line] and oneGameCurrently):
+        allGames.append(data)
+        nGame += 1
+        data = newData(nGame)
+    
+    if('ClientUserinfoChanged' in arqLines[line]):
+        if(arqLines[line].split('\\')[1] not in data['game_{}'.format(nGame)]['players']):
+            data['game_{}'.format(nGame)]['players'].append(arqLines[line].split('\\')[1])
+    
+    elif(arqLines[line].split()[1] == 'Kill:'):
+        try:
+            if(arqLines[line].split()[5] != '<world>'):
+                data['game_{}'.format(nGame)]['kills'][arqLines[line].split()[5]] += 1
+            data['game_{}'.format(nGame)]['kills'][arqLines[line].split()[7]] -= 1
+            data['game_{}'.format(nGame)]['total_kills'] += 1
+        except:
+            if(arqLines[line].split()[5] != '<world>'):
+                data['game_{}'.format(nGame)]['kills'][arqLines[line].split()[5]] = 1
+            data['game_{}'.format(nGame)]['kills'][arqLines[line].split()[7]] = -1
+
+allGames.append(data)
+
+print(allGames)
+
 # print(arqLines)
         
-navega(0,arqLines,data,nGame,oneGameCurrently)
+# navega(0,arqLines,data,nGame,oneGameCurrently)
 
 # print(arqLines[39].split())
 
